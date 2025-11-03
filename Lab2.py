6a29bd4731e43f9d5c87dd91700df00c8fab8f28
Kyrsten <aisharhaine14.24@ichat.sp.edu.sg>
2025-11-03 10:59:32 +0800

    functions

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,30 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def calc_average(temperature_list):
+    total = sum(temperature_list)
+    count = len(temperature_list)
+    avg = total / count
+    return float(avg)
+
+def get_user_input():
+    user_input = input()
+    string_list = user_input.split(",")
+    float_list = []
+    for value in string_list:
+        float_list.append(float(value.strip())) 
+    return float_list
+
+def find_min_max(temperature_list):
+    minimum = min(temperature_list)
+    maximum = max(temperature_list)
+
+    return [int(minimum), int(maximum)]
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
